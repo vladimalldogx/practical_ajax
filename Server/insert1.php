28d11a4c62023fd2ac3972aea5d9d33652258f0b
MEMEMALLEN\Vladimir <shiyocz@gmail.com>
2020-08-22 09:51:43 +0800

    same as insert.php

---
+++ Server/insert1.php
@@ -0,0 +1,23 @@
+<?php
+require ('Database.php');
+$db = new Database();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$repassword = $_POST['repassword'];
+
+
+if(empty($username) || empty($password) || empty($repassword)) {
+    echo "Empty Username, Password or Retype Password!";
+}
+else if($password != $repassword) {
+    echo "Passworrd and Retpye Password doesnt match!";
+}
+else {
+			$sql = "INSERT INTO users (username, password) VALUES ('$username','$password')";
+			$res = $db->execute($sql);
+			if($res) {
+			echo "Successfully Created!1";
+    }
+       }
+?>
